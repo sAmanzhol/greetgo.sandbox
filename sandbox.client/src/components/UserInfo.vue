5201c4cd333ce15e04deb5a39eb41755144b6c00
ekolpakov <ekolpakov@greet-go.com>
2018-08-01 13:04:44 +0600

    Подготовлен пустой UserInfo

---
+++ sandbox.client/src/components/UserInfo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="user-info">
+    <button @click="onGetUserInfo()">Получить данные пользователя</button>
+  </div>
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator';
+
+  @Component
+  export default class UserInfo extends Vue {
+    onGetUserInfo() {
+      console.log("asd")
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  button {
+    padding: .5rem;
+    color: #314050;
+  }
+</style>
--- sandbox.client/src/views/Home.vue
+++ sandbox.client/src/views/Home.vue
@@ -2,16 +2,18 @@
   <div class="home">
     <img src="../assets/sandbox.svg" width="200px">
     <HelloWorld msg="Sandbox"/>
+    <UserInfo/>
   </div>
 </template>
 
 <script lang="ts">
   import {Component, Vue} from 'vue-property-decorator';
-  import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  import HelloWorld from '@/components/HelloWorld.vue';
+  import UserInfo from '@/components/UserInfo.vue';
 
   @Component({
     components: {
-      HelloWorld,
+      HelloWorld, UserInfo,
     },
   })
   export default class Home extends Vue {
