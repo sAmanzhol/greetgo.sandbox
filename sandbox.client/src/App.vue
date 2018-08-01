38808b20ea3a08b32cee03421ef40b344f71379f
ekolpakov <ekolpakov@greet-go.com>
2018-08-01 12:40:53 +0600

    Изменены некоторые привязки

--- sandbox.client/src/App.vue
+++ sandbox.client/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
+      |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
@@ -9,21 +10,26 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  }
+
+  #nav {
+    padding: 30px;
+    a {
+      width: 5rem;
+      display: inline-block;
+      text-align: center;
+      color: #627e92;
+      &.router-link-exact-active {
+        color: #0b1c13;
+        font-weight: bold;
+
+      }
     }
   }
-}
 </style>
--- sandbox.client/src/views/About.vue
+++ sandbox.client/src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="onClick()">Click me</button>
   </div>
 </template>
+
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator';
+
+  @Component({
+    components: {},
+  })
+  export default class About extends Vue {
+    onClick() {
+      console.log("Clicked")
+    }
+  }
+</script>
