010ac8375f644585985624c3b93eb9d99642fe7b
ekolpakov <ekolpakov@greet-go.com>
2018-08-28 17:46:37 +0600

    Made good client

--- sandbox.client/src/app/app.component.ts
+++ sandbox.client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {LoginService} from "./login/login.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+//MODIFY replace sandbox {PROJECT_NAME}
+  title = 'sandbox';
+
+  constructor(public login: LoginService) {}
 }
--- sandbox.client/src/app/app.module.ts
+++ sandbox.client/src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {HttpService} from "./http.service";
+import {HttpClientModule} from "@angular/common/http";
+import {LoginService} from "./login/login.service";
+import {FormsModule} from "@angular/forms";
+import {ClientListComponent} from './client-list/client-list.component';
+import {ClientListService} from "./client-list/client-list.service";
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ClientListComponent,
+    AboutComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, FormsModule,
   ],
-  providers: [],
+  providers: [HttpService, LoginService, ClientListService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
