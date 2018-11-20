e9e1b87ffd0143e6ea05da9664a48b4eebcebeb4
ekolpakov <ekolpakov@greet-go.com>
2018-11-20 17:07:33 +0600

    war is working!!! at now it is truth

---
+++ sandbox.server/src/kz/greetgo/sandbox/server/app/SandboxServerApplication.java
@@ -0,0 +1,29 @@
+package kz.greetgo.sandbox.server.app;
+
+import kz.greetgo.sandbox.backend.util.ScannerBackend;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+@SpringBootApplication
+@Import(ScannerBackend.class)
+public class SandboxServerApplication extends SpringBootServletInitializer {
+  public static void main(String[] args) {
+    SpringApplication.run(SandboxServerApplication.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(SandboxServerApplication.class);
+  }
+
+  @Override
+  public void onStartup(ServletContext servletContext) throws ServletException {
+    super.onStartup(servletContext);
+  }
+}
