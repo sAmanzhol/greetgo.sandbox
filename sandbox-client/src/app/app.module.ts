ea035f2a6acd54a4e251be806d0b4970d65d671a
ekolpakov <ekolpakov@greet-go.com>
2018-11-21 09:47:12 +0600

    Killed app.component and created main-window.component

--- sandbox-client/src/app/app.module.ts
+++ sandbox-client/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { MainWindowComponent } from './main-window/main-window.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MainWindowComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainWindowComponent]
 })
 export class AppModule {}
